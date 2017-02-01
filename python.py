0b9c0cabff516d8007fc9da834db7d80815f4d27
Anna Matras <ania.matras@gmail.com>
2017-02-01 16:17:49 +0100

    blech method add in to do class

--- python.py
+++ python.py
@@ -2,6 +2,9 @@ class TodoItem:
     def __init__(self, name):
         self.name = name
         self.state = False
+        
+    def blech():
+        pass
 
     def toggle_done(self):
         self.state = True
